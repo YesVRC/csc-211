10f7950783ba37d39c4a7a203e347099bf6cf119
YesVRC <altair9090@gmail.com>
2025-02-25 17:34:49 -0500

    Assignment 4 and cleanups

---
+++ Assignment4/src/Shape.java
@@ -0,0 +1,15 @@
+public class Shape {
+    private int sides;
+
+    public Shape(int sides) {
+        this.sides = sides;
+    }
+
+    public int getSides() {
+        return sides;
+    }
+
+    public void setSides(int sides) {
+        this.sides = sides;
+    }
+}
